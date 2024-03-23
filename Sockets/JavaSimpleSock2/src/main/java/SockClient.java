19736f3ca52dbff0c570bd26afb895ed322e5e2d
scortezb <scortezb@asu.edu>
2024-03-22 19:52:15 -0700

    change host and adde xml user file

--- Sockets/JavaSimpleSock2/src/main/java/SockClient.java
+++ Sockets/JavaSimpleSock2/src/main/java/SockClient.java
@@ -14,7 +14,7 @@ import java.io.*;
 class SockClient {
   public static void main (String args[]) {
     Socket sock = null;
-    String host = "localhost";
+    String host = "54.81.54.127";
     String message = "HI";
     Integer number = 100;
 
